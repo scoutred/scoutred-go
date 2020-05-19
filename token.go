1368f64da8b875ddad3afb7102b7aca284de406c
Alexander Rolek <a.rolek@gmail.com>
2020-05-19 16:53:50 -0700

    Added TokenHasExpired helper and associated tests
    closes #23

---
+++ token.go
@@ -0,0 +1,20 @@
+package scoutred
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// TokenHasExpred will check if a token is expired. This function does not
+// do any other validation checks.
+func TokenHasExpired(token string) (bool, error) {
+	var claims jwt.StandardClaims
+
+	tok, parts, err := new(jwt.Parser).ParseUnverified(token, &claims)
+	if err != nil {
+		return true, err
+	}
+
+	return claims.ExpiresAt < time.Now().Unix(), nil
+}
