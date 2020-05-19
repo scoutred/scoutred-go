68cea04c1a4fef25c990fe9b0a9d19ed69e840cd
Alexander Rolek <a.rolek@gmail.com>
2020-05-19 16:53:50 -0700

    Added TokenHasExpired helper and associated tests
    closes #23

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.12
 require (
 	github.com/aws/aws-lambda-go v1.10.0
 	github.com/aws/aws-sdk-go v1.19.20
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/net v0.0.0-20200519113804-d87ec0cfa476 // indirect
 )
---
+++ token_test.go
@@ -0,0 +1,66 @@
+package scoutred_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/scoutred/scoutred-go"
+)
+
+func TestTokenHasExpired(t *testing.T) {
+	type tcase struct {
+		token      string
+		hasExpired bool
+	}
+
+	fn := func(tc tcase) func(t *testing.T) {
+		return func(t *testing.T) {
+			t.Parallel()
+
+			expired, err := scoutred.TokenHasExpired(tc.token)
+			if err != nil {
+				t.Errorf("unexepected err: %v", err)
+				return
+			}
+
+			if expired != tc.hasExpired {
+				t.Errorf("expected %v got %v", tc.hasExpired, expired)
+				return
+			}
+		}
+	}
+
+	// genTokenExpires is a helper to generate tokens and set the ExpiresAt value
+	// using a provided daysFromNow offset
+	genTokenExpires := func(daysFromNow int) string {
+		tokenPast := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+			ExpiresAt: time.Now().AddDate(0, 0, daysFromNow).Unix(), // subtract one day
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := tokenPast.SignedString([]byte("scoutred-secret"))
+		if err != nil {
+			t.Fatalf("unexepected err: %v", err)
+		}
+
+		return tokenString
+	}
+
+	// Auth handler will make sure that the route is never allowed, so
+	// no need to test for anonymous user.
+	tests := map[string]tcase{
+		"exired": {
+			token:      genTokenExpires(-1),
+			hasExpired: true,
+		},
+		"not exired": {
+			token:      genTokenExpires(1),
+			hasExpired: false,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, fn(tc))
+	}
+}
